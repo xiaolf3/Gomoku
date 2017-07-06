39e614ed1872757d9e93b480b1410fb487cab3fa
xiaolf3 <1649074439@qq.com>
2017-07-07 00:23:44 +0800

    final

---
+++ app/src/main/java/com/z/gomoku/Chess.java
@@ -0,0 +1,8 @@
+package com.z.gomoku;
+
+
+public class Chess {
+    public int who = 0;
+    public final int CHESS_WHITE = R.drawable.white;
+    public final int CHESS_BLACK = R.drawable.black;
+}
